c7c027093260428836ca2390e293e3a214dc0061
andoricano <andoricano@gmail.com>
2025-12-06 07:12:44 +0900

    [feat] Board와 UI state 연결.

--- src/module/yatch/BoardWidget.tsx
+++ src/module/yatch/BoardWidget.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image, StyleSheet, ImageSourcePropType } from 'react-native';
+import { Text, View, Image, StyleSheet, ImageSourcePropType } from 'react-native';
 import { Subscription } from 'rxjs';
 import { BoardManager } from './BoardManager';
 
@@ -12,51 +12,52 @@ interface boardWidgetProps {
 
 export class BoardWidget extends React.Component<boardWidgetProps> {
     boardManager: BoardManager;
-
     subscription!: Subscription;
 
     constructor(props: boardWidgetProps) {
         super(props);
         this.boardManager = props.boardManager;
     }
+
     state = {
-        dice: this.props.boardManager.board,
+        board: this.props.boardManager.board,
     };
 
     componentDidMount() {
-        
+        this.subscription = this.boardManager.board$.subscribe(board => {
+            this.setState({ board });
+        });
     }
 
     componentWillUnmount() {
         this.subscription.unsubscribe();
     }
 
-
-    handlePress = () => {
-        
-    };
-
-
     render() {
         const { backgroundColor = '#f0f0f0' } = this.props;
+        const { board } = this.state;
 
         return (
             <View style={[styles.container, { backgroundColor }]}>
-                
+                {Object.entries(board).map(([key, value]) => (
+                    <View key={key} style={{ margin: 5 }}>
+                        <Text>{key}: {value}</Text>
+                    </View>
+                ))}
             </View>
         );
     }
 }
-
 const styles = StyleSheet.create({
     container: {
-        flexDirection: 'row',
+        flexWrap: 'wrap',
         justifyContent: 'center',
         alignItems: 'center',
+        padding: 10,
     },
-    diceImage: {
-        width: 50,
-        height: 50,
-        marginHorizontal: 5,
+    text: {
+        fontSize: 18,
+        color: '#000',
+        marginHorizontal: 8,
     },
 });
--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import { View, Button, StyleSheet } from 'react-native';
 import { DiceWidget } from '../module/dice/DiceWidget';
 import { DiceLayout } from '../module/dice/DiceLayout';
+import { BoardWidget } from '../module/yatch/BoardWidget';
+import { initialBoard } from '../module/yatch/Board';
+import { BoardManager } from '../module/yatch/BoardManager';
 
 export class HomeScreen extends React.Component {
   handlePress = () => {
     console.log('버튼 눌림!');
   };
+  boardManager = new BoardManager(initialBoard);
 
   render() {
     return (
       <View style={styles.container}>
-        <DiceLayout width='90%' height='20%' oneLine = {false} />
+        <BoardWidget boardManager={this.boardManager} />
+        {/* <DiceLayout width='90%' height='20%' oneLine = {false} /> */}
       </View>
     );
   }
@@ -20,7 +25,10 @@ export class HomeScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: '100%',
+    height: '100%',  
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#000000'
   },
 });
